aeb3e7b79221dd705ab33c3ee3ec0c21bd5d59b9
yano <developer613@sourceplay.com>
2023-04-12 04:15:56 +0900

    Update ERC20Capped module

---
+++ contracts/erc20-capped/ERC20MaxSupply.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+
+
+contract ERC20MaxSupply is Context, AccessControlEnumerable, ERC20Burnable, ERC20Pausable {
+    
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    uint256 public immutable maxSupply;
+
+    
+    constructor(string memory name, string memory symbol, uint256 _maxSupply) ERC20(name, symbol) {
+        require(_maxSupply > 0, "MaxSupply is 0");
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(MINTER_ROLE, _msgSender());
+        _setupRole(PAUSER_ROLE, _msgSender());
+        maxSupply = _maxSupply;
+    }
+
+  
+    function mint(address to, uint256 amount) public virtual {
+        require(hasRole(MINTER_ROLE, _msgSender()), "Must have minter role to mint");
+        require(totalSupply() + amount <= maxSupply, "maxSupply exceeded");
+        _mint(to, amount);
+    }
+
+   
+    function pause() public virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()), "Must have pauser role to pause");
+        _pause();
+    }
+
+  
+    function unpause() public virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()), "Must have pauser role to unpause");
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Pausable) {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
