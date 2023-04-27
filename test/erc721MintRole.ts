ab99c4e4ce0d564d739619f1d6df376cf1b57916
yano <developer613@sourceplay.com>
2023-04-27 21:21:59 +0900

    update test script

--- test/erc721MintRole.ts
+++ test/erc721MintRole.ts
@@ -1,25 +1,47 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+import { BigNumber as BN, constants } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { time } from "@nomicfoundation/hardhat-network-helpers";
 
-describe("ERC721MintRole",  ()=> {
-    let aliceAccount, bobAccount;
-    let alice, bob;
-    let token, Token;
+const Decimals = BN.from(18);
+const OneToken = BN.from(10).pow(Decimals);
 
-    beforeEach(async () => {
-        const signers = await ethers.getSigners();
-        aliceAccount = signers[0];
-        bobAccount = signers[1];
-        alice = aliceAccount.address;
-        bob = bobAccount.address;
-        Token = await ethers.getContractFactory("ERC721MintRole");
-        token = await Token.deploy("ERC721MintRole NFT", "TTT", "https://www.test.com/");
-    })
+import {
+    ERC721MintRole,
+    ERC721MintRole__factory,
+} from "../typechain-types";
+import {max} from "hardhat/internal/util/bigint";
+import {equal} from "assert";
 
-    describe("mint", () => {
-        it("mint alice", async () => {
-            await expect(token.mint(alice, 1)).to.not.be.reverted;
-            await expect(await token.tokenURI(1)).to.be.equal('https://www.test.com/1');
+describe("Test ERC721MintRole contract",  ()=> {
+    let owner: SignerWithAddress;
+    let user1: SignerWithAddress;
+    let user2: SignerWithAddress;
+
+    let erc721Contract: ERC721MintRole;
+
+    before(async () => {
+        [owner, user1, user2] = await ethers.getSigners();
+    });
+
+    describe("1. Deploy contracts", () => {
+        it("Deploy main contracts", async () => {
+            const erc721Factory = new ERC721MintRole__factory(owner);
+            erc721Contract = await erc721Factory.deploy(
+                "ERC721MintRole NFT Token",
+                "TTT",
+                "https://www.test.com/"
+            );
         });
     });
+
+    describe("2. Test main functions", () => {
+        describe("- mint function", () => {
+            it("mint: pass", async () => {
+                await expect(erc721Contract.mint(owner.address, 1)).to.not.be.reverted;
+                await expect(await erc721Contract.tokenURI(1)).to.be.equal('https://www.test.com/1');
+            });
+        })
+    })
 });
