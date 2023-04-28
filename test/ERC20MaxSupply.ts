011901781e02184178f8ed0913b238bbaf1a4d75
yano <developer613@sourceplay.com>
2023-04-28 10:37:24 +0900

    update test script

--- test/ERC20MaxSupply.ts
+++ test/ERC20MaxSupply.ts
@@ -1,37 +1,66 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+import { ethers } from "hardhat";
+import { Signer } from "ethers";
+import { expect } from "chai";
 
-describe("ERC20MaxSupply",  ()=>{
-    let aliceAccount, bobAccount;
-    let alice, bob;
-    let token, Token;
-    const maxSupply = "100000000000000000000"
+import {
+    ERC20MaxSupply
+} from "../typechain-types";
 
-    beforeEach(async()=>{
-        const signers = await ethers.getSigners();
-        aliceAccount = signers[0];
-        bobAccount = signers[1];
-        alice = aliceAccount.address;
-        bob = bobAccount.address;
-        Token = await ethers.getContractFactory("ERC20MaxSupply");
-        token = await Token.deploy("ERC20MaxSupply test", "TTT", maxSupply);
-    })
 
-    describe("mint", ()=>{
-        it("should mint amount that keep total supply under max supply", async()=>{
-            await expect(token.mint(bob, 100)).to.not.be.reverted;
-        });
+describe("ERC20MaxSupply", () => {
+    let owner: Signer;
+    let other: Signer;
+    let token: ERC20MaxSupply;
 
-        it("should mint amount that goes over cap", async()=>{
-            let amount = "300000000000000000000"
-            await expect(token.mint(bob, amount)).to.be.revertedWith("maxSupply exceeded")
-        });
-    })
+    const name = "MyToken";
+    const symbol = "MTK";
+    const maxSupply = ethers.utils.parseEther("1000000");
 
-    describe("maxSupply", ()=>{
-        it("should check maxSupply value", async()=>{
-            let r = await token.maxSupply();
-            expect(r.toString()).to.equal(maxSupply)
-        })
-    })
-})
+    beforeEach(async () => {
+        [owner, other] = await ethers.getSigners();
+        const tokenFactory = await ethers.getContractFactory("ERC20MaxSupply");
+        token = await tokenFactory.deploy(name, symbol, maxSupply);
+
+        await token.connect(owner).mint(await other.getAddress(), ethers.utils.parseEther("1000"));
+    });
+
+    it("should have the correct name, symbol, and max supply", async () => {
+        expect(await token.name()).to.equal(name);
+        expect(await token.symbol()).to.equal(symbol);
+        expect(await token.maxSupply()).to.equal(maxSupply);
+    });
+
+    it("should allow the owner to mint tokens up to the max supply", async () => {
+        const initialSupply = await token.totalSupply();
+        const amount = ethers.utils.parseEther("10000");
+
+        await token.connect(owner).mint(await other.getAddress(), amount);
+
+        expect(await token.totalSupply()).to.equal(initialSupply.add(amount));
+    });
+
+    it("should not allow minting tokens beyond the max supply", async () => {
+        const amount = maxSupply.sub(await token.totalSupply()).add(ethers.utils.parseEther("1"));
+
+        await expect(token.connect(owner).mint(await other.getAddress(), amount)).to.be.revertedWith("maxSupply exceeded");
+    });
+
+    it("should allow the owner to pause and unpause transfers", async () => {
+        await token.connect(owner).pause();
+        expect(await token.paused()).to.be.true;
+
+        await token.connect(owner).unpause();
+        expect(await token.paused()).to.be.false;
+    });
+
+    it("should not allow non-pauser to pause or unpause transfers", async () => {
+        await expect(token.connect(other).pause()).to.be.revertedWith("Must have pauser role to pause");
+        await expect(token.connect(other).unpause()).to.be.revertedWith("Must have pauser role to unpause");
+    });
+
+    it("should not allow burning tokens beyond the balance", async () => {
+        const balance = await token.balanceOf(await other.getAddress());
+
+        await expect(token.connect(other).burn(balance.add(ethers.utils.parseEther("1")))).to.be.revertedWith("ERC20: burn amount exceeds balance");
+    });
+});
