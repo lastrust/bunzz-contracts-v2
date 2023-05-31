670b9596d55e13d1172f8fe5953c5bdd17421f7c
smart-ticky <developer613@sourceplay.com>
2023-05-31 18:01:32 +0700

    update test script

--- contracts/mock/MockERC1155.sol
+++ contracts/mock/MockERC1155.sol
@@ -3,14 +3,33 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract MockERC1155 is ERC1155 {
-    using SafeMath for uint256;
+contract MockERC1155 is ERC1155, Ownable {
+    using Strings for uint256;
 
-    constructor(string memory uri_) ERC1155(uri_) {}
+    string public baseURI;
 
-    function mint(address to, uint256 id, uint256 amount, bytes memory data) external {
-        _mint(to, id, amount, data);
+    constructor(string memory _baseURI) ERC1155(_baseURI) {
+        baseURI = _baseURI;
+    }
+
+    function setBaseURI(string memory _baseURI) public onlyOwner {
+        baseURI = _baseURI;
+        _setURI(_baseURI);
+    }
+
+    function uri(uint256 tokenId) public view override returns (string memory) {
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
+    }
+
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public onlyOwner {
+        _mint(account, id, amount, data);
     }
 }
